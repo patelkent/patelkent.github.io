7357e0b7095ca315b9bf547c6a82329b9a4f537b
patelkent <141389123+patelkent@users.noreply.github.com>
2026-01-22 15:41:20 +0530

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-function toggleMobileMenu() {
-  const menu = document.getElementById("mobile-menu");
-  if (menu) menu.classList.toggle("hidden");
-}
-
-function scrollToContact() {
-  const section = document.getElementById("contact");
-  if (section) section.scrollIntoView({ behavior: "smooth" });
-}
